bcfe86c32de5c6537d11813ebd6795998ef803ff
charlie <science-35@outlook.com>
2022-04-08 18:11:28 +0800

    not an working update but revised platform alike ideas APPs the necessity

--- PID.c
+++ PID.c
@@ -5,11 +5,15 @@ int inter(int d, int tickms)
     static int I = 0;
     int i = I;
     I = I + d * tickms;
-    if(d<0){
-        if(I>i){
+    if (d < 0)
+    {
+        if (I > i)
+        {
             I = -(__INT32_MAX__);
         }
-    }else if(I<i){
+    }
+    else if (I < i)
+    {
         I = __INT32_MAX__;
     }
     return I;
@@ -27,47 +31,58 @@ typedef struct PID
     int output;
     int *outaddr;
     int *inaddr;
-}PID;
+} PID;
 
-PID*PIDinit(int*inaddr,int*outaddr,int p ,int i,int d){
-    PID*temp = (PID*)malloc(sizeof(PID));
+PID *PIDinit(int *inaddr, int *outaddr, int p, int i, int d)
+{
+    PID *temp = (PID *)malloc(sizeof(PID));
     temp->p = p;
     temp->i = i;
     temp->d = d;
     temp->inaddr = inaddr;
     temp->outaddr = outaddr;
-    temp->I=0;
-    temp->last=0;
+    temp->I = 0;
+    temp->last = 0;
     return temp;
 }
 
-void PIDupdate(PID*p,int tick){
-    int Err = p->setpoint - p->current;
-    int D = (Err - p->last)/tick;
-    p->last = Err;
-    p->I=inter(Err,tick);
-    p->output = (p->p*(Err+(p->i)*(p->I)+(p->d)*D))/1000;
+void PID_IO(PID *p)
+{
+    p->current = *(p->inaddr);
+    *(p->outaddr) = p->output;
 }
+
+void PIDupdate(PID *p, int tick)
+{
+    
+    int Err = p->setpoint - p->current;
+    int D = (Err - p->last) / tick;
+    p->last = Err;
+    p->I = inter(Err, tick);
+    p->output = (p->p * (Err + (p->i) * (p->I) + (p->d) * D)) / 1000;
+    PID_IO(p);
+}
+
+
 
 int main()
 {
     int din;
     int dout;
     int tick;
-    FILE * Din;
-    freopen("./1.txt","r",Din);
-    FILE * Dout;
-    freopen("./2.txt","w",Dout);
-    PID*pid1 = PIDinit(&din,&dout,100,10,10);
+    FILE *Din;
+    freopen("./Din.txt", "r", Din);
+    FILE *Dout;
+    freopen("./Dout.txt", "w", Dout);
+    PID *pid1 = PIDinit(&din, &dout, 100, 10, 10);
     while (feof(Din))
     {
-        fscanf(Din,"%d,%d",&din,&tick);
-        pid1->current=din;
-        PIDupdate(pid1,tick);
+        fscanf(Din, "%d,%d", &din, &tick);
+        pid1->current = din;
+        PIDupdate(pid1, tick);
         dout = pid1->output;
-        fprintf(Dout,"%d",dout);
+        fprintf(Dout, "%d", dout);
     }
     fclose(Din);
     fclose(Dout);
-
 }
--- datagen.py
+++ datagen.py
@@ -1,2 +1,12 @@
-import numpy as np
+from numpy import *
+x = arange(0,10,0.01)
+y = 1000*sin(x)
+din = open("./Dout.txt","w")
+for i in y:
+    a = int(i)
+    b = str(a)
+    din.write(b)
+    din.write(",")
+    din.write("12\n")
+din.close
 
